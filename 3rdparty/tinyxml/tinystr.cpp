f43eb7a4167d94dd412fe723e120bbb0222c5879
anael.seghezzi@gmail.com <anael.seghezzi@gmail.com@9d9a707f-7efe-918e-91e4-5b34a9e3d4e4>
2013-02-23 17:15:34 +0000

    xml level : special characters

--- 3rdparty/tinyxml/tinystr.cpp
+++ 3rdparty/tinyxml/tinystr.cpp
@@ -11,7 +11,7 @@ purpose, including commercial applications, and to alter it and
 redistribute it freely, subject to the following restrictions:
 
 1. The origin of this software must not be misrepresented; you must
-not claim that you wrote the original software. If you use this
+not claim that you wrote the original software. If you use th is
 software in a product, an acknowledgment in the product documentation
 would be appreciated but is not required.
 
